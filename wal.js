0757edb7e5f8f6c55ed358603dc837d869566f60
1850837 <a1850837@adelaide.edu.au>
2026-07-29 21:16:59 +0930

    Working on wagging the tail

---
+++ wal.js
@@ -0,0 +1,20 @@
+// Dark mode
+
+// Wag tail
+const dogBody = document.getElementById("dogbody");
+
+dogBody.addEventListener("mouseenter", (event) => {
+    let tail = document.getElementById("dogtail");
+    tail.src = "/Art/Crayons Wallace Tail 2.png";
+})
+
+dogBody.addEventListener("mouseleave", (event) => {
+    let tail = document.getElementById("dogtail");
+    tail.src = "/Art/Crayons Wallace Tail 1.png";
+})
+
+// Return tail to normal
+
+// Bark
+
+// Text?
